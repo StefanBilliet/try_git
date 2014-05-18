b78ea8297deefba64049941265c3e543618df8c4
sbillietcsc <sbilliet@csc.com>
2014-05-18 10:50:28 +0200

    Committing file

---
+++ CustomBindingHandlers.js
@@ -0,0 +1,35 @@
+﻿(function (ko) {
+  "use strict";
+
+  // create binding handler for sliding views
+  ko.bindingHandlers.showResults = (function () {
+    var func = function (element, valueAccessor) {
+      var visible = ko.utils.unwrapObservable(valueAccessor());
+      if (visible) {
+        $(element).patientsearch('slideOpen');
+      } else {
+        $(element).patientsearch('slideClosed');
+      }
+    };
+    return {
+      init: func,
+      update: func
+    };
+  }());
+
+  ko.bindingHandlers.showDetail = (function () {
+    var func = function (element, valueAccessor) {
+      var visible = ko.utils.unwrapObservable(valueAccessor());
+      if (visible) {
+        $('#searchPatientTab').patientsearch('showDetail', $(element));
+      } else {
+        $('#searchPatientTab').patientsearch('hideDetail');
+      }
+    };
+    return {
+      init: func,
+      update: func
+    };
+  }());
+
+}(window.ko));
